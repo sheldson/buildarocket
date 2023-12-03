b0c51c42049dfe414f8db449b7c98f946a4be883
sheldon <sheldson027@gmail.com>
2023-12-03 23:05:29 +0800

    Refactor node-fetch to use dynamic import for ESM compatibility

--- api/whois.js
+++ api/whois.js
@@ -1,5 +1,5 @@
 // api/whois.js
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 module.exports = async (request, response) => {
     const { domain, suffix } = request.query;
